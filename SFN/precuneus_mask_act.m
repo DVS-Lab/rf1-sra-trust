59948b98a7d71ef4651c3541d99e1b37cfb87537
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-06 21:17:51 -0500

    Updatest to poster

---
+++ SFN/precuneus_mask_act.m
@@ -0,0 +1,25 @@
+input_data = readtable("/Users/avidachs/Documents/GitHub/rf1-sra-data/v2.3_SFN_Covariates.xlsx");
+
+% Extract the 'sub_age' and 'oafem_total' columns
+sub_age = input_data.sub_age;
+oafem_total = input_data.precuneus_mask_activation;
+
+% Create a scatter plot without 'filled'
+scatter(sub_age, oafem_total);
+title('Scatter Plot of sub_age vs. oafem_total');
+xlabel('age');
+ylabel('Precuneus Act');
+grid on;
+
+% Fit a linear regression line
+coefficients = polyfit(sub_age, oafem_total, 1);
+line_fit = polyval(coefficients, sub_age);
+
+% Add the line of best fit to the plot
+hold on;
+plot(sub_age, line_fit, 'r', 'LineWidth', 2); % Decreased line width
+legend('Data', 'Line of Best Fit');
+hold off;
+
+disp(class(sub_age));
+disp(class(oafem_total));
