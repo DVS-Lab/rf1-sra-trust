3018c73983464f016ef2924f0bd1a1fd11f529f8
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-08 18:34:28 -0500

    changed scatterplot code to work for iosXage

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,14 +3,14 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/IOS/iosXageFC.xlsx"
-data <- read_excel(file_path)
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/trustBehavior.csv"
+data <- read.csv(file_path)
 
 # Fit a linear model
-lm_model <- lm(iosF_C ~ age, data)
+lm_model <- lm(fs ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = iosF_C)) +
+ggplot(data, aes(x = age, y = fs)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
