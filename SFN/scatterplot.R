c71f1d3798cad46dc5b5e0470bfa7abfbba96743
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 18:21:50 -0500

    OAFEM scatterplots

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/IOS/IOS.csv"
-data <- read.csv(file_path)
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/OAFEM/oafemScatter.xlsx"
+data <- read_excel(file_path)
 
 # Fit a linear model
-lm_model <- lm(fc_ios ~ age, data)
+lm_model <- lm(oafem ~ fc_ios, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = fc_ios)) +
+ggplot(data, aes(x = fc_ios, y = oafem)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
-  labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
-       x = "age",
-       y = "fc_ios")
+  labs(title = "Scatterplot",
+       x = "IOS F-C",
+       y = "OAFEM")
 
 summary(lm_model)
