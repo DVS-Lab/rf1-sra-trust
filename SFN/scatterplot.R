d01ffd1df62df80e4b06d312683bf5e6e36ae52b
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 12:19:28 -0500

    generated every trust behav X age plot
    no significant results

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Images/Graphs/accXageC16.xlsx"
-data <- read_xlsx(file_path)
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/trust behavior/trustBehavior.csv"
+data <- read.csv(file_path)
 
 # Fit a linear model
-lm_model <- lm(c17 ~ age, data)
+lm_model <- lm(fs_recip ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = c17)) +
+ggplot(data, aes(x = age, y = fs_recip)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE) +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
        x = "age",
-       y = "Activation")
+       y = "fs_recip")
 
 summary(lm_model)
