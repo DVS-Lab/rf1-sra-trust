6b1428aa607f1af12efac01ee8b159472d78ff73
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 09:39:44 -0500

    made pdf version of all of the vs X age

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/C10Xfs.xlsx"
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/copesXage.xlsx"
 data <- read_xlsx(file_path)
 
 # Fit a linear model
-lm_model <- lm(C10 ~ f-s, data)
+lm_model <- lm(C10 ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = f-s, y = C10)) +
+ggplot(data, aes(x = age, y = C10)) +
   geom_point() +
-  stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
+  stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE) +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
-       x = "f-s",
+       x = "age",
        y = "Cope10")
 
 summary(lm_model)
