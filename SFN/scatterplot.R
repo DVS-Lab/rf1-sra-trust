e683ab1cc0a2ac2f0da0a034d1f62e1715c24688
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-08 20:38:48 -0500

    OHHHH YEAAAAAHHH

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/trustBehavior.csv"
-data <- read.csv(file_path)
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/C10Xfs.xlsx"
+data <- read_xlsx(file_path)
 
 # Fit a linear model
-lm_model <- lm(fs ~ age, data)
+lm_model <- lm(C10 ~ f-s, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = fs)) +
+ggplot(data, aes(x = f-s, y = C10)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
-       x = "Age",
-       y = "iosF_C")
+       x = "f-s",
+       y = "Cope10")
 
 summary(lm_model)
