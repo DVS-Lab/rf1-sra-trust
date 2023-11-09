03e5a00708b0d50e92ebb8d3632e22a2ab870b6a
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 11:04:51 -0500

    created the vs x age images

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/copesXage.xlsx"
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Images/Graphs/accXageC16.xlsx"
 data <- read_xlsx(file_path)
 
 # Fit a linear model
-lm_model <- lm(C10 ~ age, data)
+lm_model <- lm(c17 ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = C10)) +
+ggplot(data, aes(x = age, y = c17)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE) +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
        x = "age",
-       y = "Cope10")
+       y = "Activation")
 
 summary(lm_model)
