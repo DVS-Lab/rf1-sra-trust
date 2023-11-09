12c2e561162ac4d93f5b1ab07217916c850301f2
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 13:07:52 -0500

    Added age activation brain images to posete

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/trust behavior/trustBehavior.csv"
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/IOS/IOS.csv"
 data <- read.csv(file_path)
 
 # Fit a linear model
-lm_model <- lm(fs_recip ~ age, data)
+lm_model <- lm(fc_ios ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = fs_recip)) +
+ggplot(data, aes(x = age, y = fc_ios)) +
   geom_point() +
-  stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE) +
+  stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
   labs(title = "Scatterplot of Age vs. iosF_C with Error Bars",
        x = "age",
-       y = "fs_recip")
+       y = "fc_ios")
 
 summary(lm_model)
