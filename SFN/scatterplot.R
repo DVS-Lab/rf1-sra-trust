c22f90edbf0603a843ad492d3e7bd7009934540c
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-09 19:32:45 -0500

    updated VS mask plot

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,18 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Trust Behavioral Analysis/trust behavior/trustBehavior.csv"
-data <- read_excel(file_path)
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/copesXage.xlsx"
+data <- read.csv(file_path)
 
 # Fit a linear model
-lm_model <- lm(fs_recip ~ age, data)
+lm_model <- lm(C10 ~ age, data)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = fs_recip)) +
+ggplot(data, aes(x = age, y = C10)) +
   geom_point() +
-  stat_smooth(method = "lm", formula = y ~ x, color = "blue", se = TRUE, linetype = "dotted") +
+  stat_smooth(method = "lm", formula = y ~ x, color = "red", se = TRUE, linetype = "dotted") +
   labs(title = "Scatterplot",
        x = "age",
-       y = "fs_recip")
+       y = "C10")
 
 summary(lm_model)
