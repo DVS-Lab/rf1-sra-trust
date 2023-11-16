e5a469bc8a89b3373b79d516fd236106fa874450
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-16 12:17:10 -0500

    final push

--- SFN/scatterplot.R
+++ SFN/scatterplot.R
@@ -3,18 +3,22 @@ library(ggplot2)
 library(broom)
 
 # Read the data from the Excel file
-file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/Images/Graphs/Whole Brain Activation X Age/activationXageC16.xlsx"
+file_path <- "/Users/avidachs/Documents/GitHub/rf1-sra-trust/SFN/covariates/oafemScatter.xlsx"
 data <- read_excel(file_path)
 
+
 # Fit a linear model
-lm_model <- lm(c17 ~ age, data)
+lm_model <- lm(oafem ~ age, data)
+
+#correlation coefficient
+cor_coef <- cor(data$oafem, data$age)
 
 # Create the scatterplot with a line of best fit and error bars
-ggplot(data, aes(x = age, y = c17)) +
+ggplot(data, aes(x = age, y = oafem)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y ~ x, color = "red", se = TRUE) +
-  labs(title = "Scatterplot",
+  labs(title = paste("Scatterplot (Correlation: ", round(cor_coef, 3), ")"),
        x = "age",
-       y = "C10")
+       y = "c16")
 
 summary(lm_model)
