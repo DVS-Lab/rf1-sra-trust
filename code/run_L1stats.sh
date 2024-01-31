c2c3aaacd9fa2a4e3814993c6accc98e7d920606
Cooper Joseph Sharp <tun31934@temple.edu>
2024-01-31 18:57:39 -0500

    Update sublist and stats scripts for future use after troubleshooting

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -3,14 +3,14 @@
 # Ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
-nruns=2
+#nruns=2
 
 #for task in socialdoors; do
 for task in trust; do
 	for ppi in 0 "VS"; do #"VS_thr5" "dmn"; do # 0 "VS_thr5" "dmn"; do # putting 0 first will indicate "activation"
-		for sub in 10668; do #-- use this line for testing with one subject 
-		#for sub in `cat ${basedir}/code/sublist_all.txt`; do
-	  		for run in `seq $nruns`; do
+		#for sub in 10668; do #-- use this line for testing with one subject 
+		for sub in `cat ${basedir}/code/sublist_all.txt`; do
+	  		for run in 1 2; do
 
 		  		# Manages the number of jobs and cores
 		  		SCRIPTNAME=${basedir}/code/L1stats.sh
