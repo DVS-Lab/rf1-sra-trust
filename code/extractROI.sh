4964ae8ac93208a9916da0548dd9c9ae2b83d46c
Cooper Joseph Sharp <tun31934@temple.edu>
2024-03-05 11:18:47 -0500

    Update extract script and add VS activation for current models

--- code/extractROI.sh
+++ code/extractROI.sh
@@ -12,17 +12,20 @@ for TYPE in act; do
 	for ROI in seed-VS; do
 		MASK=${maindir}/masks/${ROI}.nii.gz
 		TASK=trust
-		outputdir=${maindir}/derivatives/imaging_plots
+		group=onegroup
+		N=87
+		outputdir=${maindir}/derivatives/imaging_plots/
 		mkdir -p $outputdir
 
-		for model in age; do 
-			for copeinfo in "4 C_def" "5 C_rec" "6 F_def" "7 F_rec" "8 S_def" "9 S_rec" "10 rec-def" "11 face" "12 rec-def_F-S" "13 F-S" "14 F-C" "15 S-C" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose"; do
-				# split copeinfo variable
+		for model in 4; do 
+			#for copeinfo in "4 C_def" "5 C_rec" "6 F_def" "7 F_rec" "8 S_def" "9 S_rec" "10 rec-def" "11 face" "12 rec-def_F-S" "13 F-S" "14 F-C" "15 S-C" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose"; do
+			for copeinfo in "10 rec-def" "12 rec-def_F-S" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose"; do	
+				# split copeinfo variable				
 				set -- $copeinfo
 				copenum=$1
 				copename=$2
-				MAINOUTPUT=${maindir}/derivatives/fsl/L3act/L3_model-${model}_task-trust_n62_flame1
-				DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_${TYPE}_cnum-${copenum}_cname-${copename}_twogroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+				MAINOUTPUT=${maindir}/derivatives/fsl/L3act/L3_model-${model}_task-trust_n${N}_flame1
+				DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_${TYPE}_cnum-${copenum}_cname-${copename}_${group}.gfeat/cope1.feat/filtered_func_data.nii.gz`
 				fslmeants -i $DATA -o ${outputdir}/${ROI}_model-${model}_type-${TYPE}_cope-${copenum}_cname-${copename}.txt -m ${MASK}
 			done
 		done
