53a59e5fa1538e62b2fd9a10ecf0493d303f68c7
Cooper Joseph Sharp <tun31934@temple.edu>
2024-03-05 19:17:49 -0500

    Update tsnr script and run for all current subs

---
+++ code/tsnr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Set the number of cores to use
+NCORES=20
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#maindir="$(dirname "$scriptdir")"
+
+# Define the data directory
+datadir=/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep
+task=trust
+
+# Define a function to process a single subject
+process_subject() {
+    sub=$1
+    cd "$datadir/sub-${sub}/func" || return
+    for i in sub-${sub}_task-${task}*_bold.nii.gz; do
+        fslmaths "$i" -Tmean tmp_mean
+        fslmaths "$i" -Tstd tmp_std
+        fslmaths tmp_mean -div tmp_std tmp_tsnr
+        fslmaths tmp_tsnr -thr 2 thr_tmp_tsnr
+        max=$(fslstats thr_tmp_tsnr -R | awk '{ print $2 }')
+        mean=$(fslstats thr_tmp_tsnr -M)
+        echo -e "$i\t mean tsnr: $mean\t max tsnr: $max"
+    done
+}
+
+# Read the subject list and process subjects in parallel
+while IFS= read -r sub; do
+	NCORES=20
+    process_subject "$sub" &
+    if [[ $(jobs -r -p | wc -l) -ge $NCORES ]]; then
+        wait -n
+    fi
+done < /ZPOOL/data/projects/rf1-sra-trust/code/sublist_all.txt
+
+# Wait for all remaining jobs to finish
+wait
