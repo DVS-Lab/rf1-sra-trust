7d92d37434ebcb20ff6efe4b2320c6f1682f2a02
cooper-sharp <tun31934@temple.edu>
2024-03-05 23:05:47 -0500

    Add tsnr by age script and clean VS input sheet

--- code/tsnr_by_age.R
+++ code/tsnr_by_age.R
@@ -2,16 +2,22 @@ library(ggplot2)
 library(dplyr)
 
 # Read the data from the .tsv file
-df <- read.delim("/Users/coopersharp/Documents/GitHub/rf1-sra-trust/code/tsnr_trust.tsv", header = TRUE, sep = "\t")
+df <- read.delim("/Users/coopersharp/Documents/GitHub/rf1-sra-trust/code/tsnr_trust_VS.tsv", header = TRUE, sep = "\t")
 df <- na.omit(df)
+
 
 # Extract subid from 'sub' column
 df$subid <- as.numeric(gsub(".*sub-(\\d+).*", "\\1", df$sub))
+df$echo <- as.numeric(gsub(".*echo-(\\d+).*", "\\1", df$sub))
+
+# Filter data where echo = 1
+df <- df %>%
+  filter(echo == 1)
 
 # Calculate mean tSNR for each subject
 subject_summary <- df %>%
   group_by(subid) %>%
-  summarize(mean_tsnr = mean(mean))
+  summarize(mean_tsnr = mean(meanVS))
 
 # Read the list of subject numbers and ages from the CSV file
 age_data <- read.csv("/Users/coopersharp/Desktop/age_tsnr.csv", header = TRUE)
@@ -23,7 +29,7 @@ subject_summary <- merge(subject_summary, age_data, by = "subid")
 ggplot(subject_summary, aes(x = age, y = mean_tsnr)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
-  labs(title = "Mean of tSNR for All Echoes Averaged for Each Subject", x = "Age", y = "Mean of Mean tSNR") +
+  labs(title = "Mean of tSNR for Echo 1 for Each Subject", x = "Age", y = "Mean of VS tSNR") +
   theme_minimal()
 
 # Calculate correlation coefficient
