181126eb7b9749b5af97b3ea6a31260212bc3320
cooper-sharp <tun31934@temple.edu>
2024-04-29 09:29:21 -0400

    Remove  and add torque launch command

--- code/L2stats-hpc.sh
+++ code/L2stats-hpc.sh
@@ -29,42 +29,47 @@ model=1
 NCOPES=18
 
 
-# ppi has more contrasts than act (phys), so need a different L2 template
-if [ "${type}" == "act" ]; then
-	ITEMPLATE=/home/tun31934/work/rf1-sra-trust/templates/L2_task-trust_model-${model}_type-act_nruns-2.fsf
-	NCOPES=${NCOPES}
-else
-	ITEMPLATE=/home/tun31934/work/rf1-sra-trust/templates/L2_task-trust_model-${model}_type-ppi_nruns-2.fsf
-	let NCOPES=${NCOPES}+1 # add 1 since we tend to only have one extra contrast for PPI
-fi
-INPUT1=${MAINOUTPUT}/L1_task-trust_model-${model}_type-${type}_run-1_sm-${sm}.feat
-INPUT2=${MAINOUTPUT}/L1_task-trust_model-${model}_type-${type}_run-2_sm-${sm}.feat
-# --- end EDIT HERE end: exceptions and conditionals for the task; need to exclude bad/missing runs
+for sub in ${subjects[@]}; do
+	
+	# ppi has more contrasts than act (phys), so need a different L2 template
+	if [ "${type}" == "act" ]; then
+		ITEMPLATE=/home/tun31934/work/rf1-sra-trust/templates/L2_task-trust_model-${model}_type-act_nruns-2.fsf
+		NCOPES=${NCOPES}
+	else
+		ITEMPLATE=/home/tun31934/work/rf1-sra-trust/templates/L2_task-trust_model-${model}_type-ppi_nruns-2.fsf
+		let NCOPES=${NCOPES}+1 # add 1 since we tend to only have one extra contrast for PPI
+	fi
+	INPUT1=${MAINOUTPUT}/L1_task-trust_model-${model}_type-${type}_run-1_sm-${sm}.feat
+	INPUT2=${MAINOUTPUT}/L1_task-trust_model-${model}_type-${type}_run-2_sm-${sm}.feat
+	# --- end EDIT HERE end: exceptions and conditionals for the task; need to exclude bad/missing runs
+	
+	
+	# check for existing output and re-do if missing/incomplete
+	OUTPUT=${MAINOUTPUT}/L2_task-trust_model-${model}_type-${type}_sm-${sm}
+	if [ -e ${OUTPUT}.gfeat/cope${NCOPES}.feat/cluster_mask_zstat1.nii.gz ]; then # check last (act) or penultimate (ppi) cope
+		echo "skipping existing output"
+	else
+		echo "re-doing: ${OUTPUT}" >> re-runL2.log
+		rm -rf ${OUTPUT}.gfeat
+	
+		# set output template and run template-specific analyses
+		OTEMPLATE=${MAINOUTPUT}/L2_task-trust_model-${model}_type-${type}.fsf
+		sed -e 's@OUTPUT@'$OUTPUT'@g' \
+		-e 's@INPUT1@'$INPUT1'@g' \
+		-e 's@INPUT2@'$INPUT2'@g' \
+		<$ITEMPLATE> $OTEMPLATE
+		feat $OTEMPLATE
+	
+		# delete unused files
+		for cope in `seq ${NCOPES}`; do
+			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
+			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
+			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
+			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
+			rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
+		done
+	fi
 
+done
 
-# check for existing output and re-do if missing/incomplete
-OUTPUT=${MAINOUTPUT}/L2_task-trust_model-${model}_type-${type}_sm-${sm}
-if [ -e ${OUTPUT}.gfeat/cope${NCOPES}.feat/cluster_mask_zstat1.nii.gz ]; then # check last (act) or penultimate (ppi) cope
-	echo "skipping existing output"
-else
-	echo "re-doing: ${OUTPUT}" >> re-runL2.log
-	rm -rf ${OUTPUT}.gfeat
-
-	# set output template and run template-specific analyses
-	OTEMPLATE=${MAINOUTPUT}/L2_task-trust_model-${model}_type-${type}.fsf
-	sed -e 's@OUTPUT@'$OUTPUT'@g' \
-	-e 's@INPUT1@'$INPUT1'@g' \
-	-e 's@INPUT2@'$INPUT2'@g' \
-	<$ITEMPLATE> $OTEMPLATE
-	feat $OTEMPLATE
-
-	# delete unused files
-	for cope in `seq ${NCOPES}`; do
-		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
-		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
-		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
-		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
-		rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
-	done
-
-fi
+torque-launch -p $logdir/chk_feat_${PBS_JOBID}.txt $logdir/cmd_feat_${PBS_JOBID}.txt
