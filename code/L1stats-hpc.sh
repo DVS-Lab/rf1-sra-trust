b8fe6910aca172fffeccf4cb82304e9fe4b35282
cooper-sharp <tun31934@temple.edu>
2024-04-29 13:53:04 -0400

    Added back mkdir line for reg subdirectory

--- code/L1stats-hpc.sh
+++ code/L1stats-hpc.sh
@@ -163,11 +163,10 @@ for sub in ${subjects[@]}; do
 
 # fix registration as per NeuroStars post:
 # https://neurostars.org/t/performing-full-glm-analysis-with-fsl-on-the-bold-images-preprocessed-by-fmriprep-without-re-registering-the-data-to-the-mni-space/784/3
-# mkdir -p ${OUTPUT}.feat/reg
+mkdir -p ${OUTPUT}.feat/reg
 ln -s $FSLDIR/etc/flirtsch/ident.mat ${OUTPUT}.feat/reg/example_func2standard.mat
 ln -s $FSLDIR/etc/flirtsch/ident.mat ${OUTPUT}.feat/reg/standard2example_func.mat
 ln -s ${OUTPUT}.feat/mean_func.nii.gz ${OUTPUT}.feat/reg/standard.nii.gz
-
 # delete unused files
 rm -rf ${OUTPUT}.feat/stats/res4d.nii.gz
 rm -rf ${OUTPUT}.feat/stats/corrections.nii.gz
