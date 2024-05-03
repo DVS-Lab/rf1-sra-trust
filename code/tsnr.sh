f6c70df0153119d660e06e6532337ef1bfe5462c
cooper-sharp <tun31934@temple.edu>
2024-05-03 11:11:08 -0400

    Add apply transforms -- getting empty mask error

--- code/tsnr.sh
+++ code/tsnr.sh
@@ -17,26 +17,36 @@ echo -e "sub\tmean\tmax\tmeanVS\t"
 process_subject() {
     sub=$1
     cd "$datadir/sub-${sub}/func" || return
-    for i in sub-${sub}_task-${task}*_echo-*_bold.nii.gz; do
+    
+    # Apply transformation using antsApplyTransforms
+    antsApplyTransforms -d 3 \
+    -i /ZPOOL/data/projects/rf1-sra-trust/masks/VS-Imanova_2mm.nii \
+    -r /ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${task}_run-1_echo-1_desc-preproc_bold.nii.gz \
+    -t /ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep/sub-${sub}/anat/sub-${sub}_from-MNI152NLin6Asym_to-T1w_mode-image_xfm.h5 \
+    -t [/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${task}_run-1_from-boldref_to-T1w_mode-image_desc-coreg_xfm.txt, 1] \
+    -n Linear \
+    -o /ZPOOL/data/projects/rf1-sra-trust/masks/submasks/sub-${sub}_task-${task}_run-1_space-native_roi-vs_mask.nii.gz
+    
+    for i in sub-${sub}_task-${task}_run-1_echo-*_desc-preproc_bold.nii.gz; do
         fslmaths "$i" -Tmean tmp_mean
         fslmaths "$i" -Tstd tmp_std
         fslmaths tmp_mean -div tmp_std tmp_tsnr
         fslmaths tmp_tsnr -thr 2 thr_tmp_tsnr
         max=$(fslstats thr_tmp_tsnr -R | awk '{ print $2 }')
         mean=$(fslstats thr_tmp_tsnr -M)
-        vsmean=$(fslstats thr_tmp_tsnr -k /ZPOOL/data/projects/rf1-sra-trust/masks/seed-VS_preproc.nii.gz -M)
+        vsmean=$(fslstats thr_tmp_tsnr -k /ZPOOL/data/projects/rf1-sra-trust/masks/submasks/sub-${sub}_task-${task}_run-1_space-native_roi-vs_mask.nii.gz -M)
         echo -e "$i\t $mean\t $max\t $vsmean"
     done
 }
 
 # Read the subject list and process subjects in parallel
 while IFS= read -r sub; do
-	NCORES=20
+    NCORES=20
     process_subject "$sub" &
     if [[ $(jobs -r -p | wc -l) -ge $NCORES ]]; then
         wait -n
     fi
-done < /ZPOOL/data/projects/rf1-sra-trust/code/sublist_all.txt
+done < /ZPOOL/data/projects/rf1-sra-trust/code/sublist_h5.txt
 
 # Wait for all remaining jobs to finish
 wait
