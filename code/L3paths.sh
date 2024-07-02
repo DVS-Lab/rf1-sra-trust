a520d13fbc41325867d92313821b71b25c949a8f
cooper-sharp <tun31934@temple.edu>
2024-07-02 10:07:02 -0400

    Add script and output for L3 inputs

--- code/L3paths.sh
+++ code/L3paths.sh
@@ -8,7 +8,7 @@ sublist="/ZPOOL/data/projects/rf1-sra-trust/code/sublist_all.txt"
 # Loop through each subject in the list
 while IFS= read -r subject; do
 
-    files=$(ls -1 "/ZPOOL/data/projects/rf1-sra-trust/derivatives/fsl/sub-$subject/L2_task-trust_model-1_type-act_sm-5.gfeat/cope4.feat/stats/cope1.nii.gz" 2>/dev/null)
+    files=$(ls -1 "/ZPOOL/data/projects/rf1-sra-trust/derivatives/fsl/sub-$subject/L2_task-trust_model-1_type-act_seed-0_sm-5.gfeat/cope4.feat/stats/cope1.nii.gz" 2>/dev/null)
 
     if [ -z "$files" ]; then
         files=$(ls -1 "/ZPOOL/data/projects/rf1-sra-trust/derivatives/fsl/sub-$subject/L1_task-trust_model-1_type-act_run-1_sm-5.feat/stats/cope1.nii.gz" 2>/dev/null)
